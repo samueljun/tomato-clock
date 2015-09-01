d5170bfc0642127a0e1f4ab66d361d4b35f5683e
Samuel Jun <mrsamueljun@gmail.com>
2015-09-01 12:17:12 -0700

    Fix Buggy Async Alarm Code for Chromium
    Added support for promises.

--- Source/Platform/Chromium/panel.browser.js
+++ Source/Platform/Chromium/panel.browser.js
@@ -7,24 +7,54 @@ function openTab(link) {
 }
 
 function resetBrowserTimer() {
-	chrome.alarms.getAll(function(alarms) {
-		for (var i = 0; i < alarms.length; i++) {
-			if (alarms[i].name.startsWith(pomodoroAlarmNamespace)) {
-				chrome.alarms.clear(alarms[i].name);
+	var promise = new Promise(function(resolve, reject) {
+		chrome.alarms.getAll(function(alarms) {
+			var promises = [];
+
+			for (var i = 0; i < alarms.length; i++) {
+				var alarmName = alarms[i].name;
+
+				if (alarmName.startsWith(pomodoroAlarmNamespace)) {
+					promises.push(new Promise(function(resolve1, reject1) {
+						chrome.alarms.clear(alarmName, function(wasCleared) {
+							resolve1();
+						});
+					}));
+				}
 			}
-		}
+
+			Promise.all(promises).then(function(value) {
+				resolve();
+			}, function(reason) {
+				resolve();
+			});
+		});
 	});
+
+	return promise;
 }
 
 function setBrowserTimer(ms) {
 	var minutes = ms / 60000;
+	var promise = resetBrowserTimer();
 
-	resetBrowserTimer();
-	chrome.alarms.create(pomodoroAlarmNamespace + '.' + minutes, {
-		delayInMinutes: minutes
+	promise.then(function(value) {
+		chrome.alarms.create(pomodoroAlarmNamespace + '.' + minutes, {
+			delayInMinutes: minutes
+		});
+
+		chrome.alarms.getAll(function(alarms) {
+			for (var i = 0; i < alarms.length; i++) {
+				var alarmName = alarms[i].name;
+			}
+		});
+	}, function(reason) {
+		console.log('resetBrowserTimer() promise rejected: ' + reason);
 	});
+
 }
 
+// Initialize popup with time text
 chrome.alarms.getAll(function(alarms) {
 	for (var i = 0; i < alarms.length; i++) {
 		if (alarms[i].name.startsWith(pomodoroAlarmNamespace)) {
