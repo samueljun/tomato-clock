5e7a241cbb9a96b2662002ceef6c2867d693e0ff
Samuel Jun <sjun@uber.com>
2017-01-30 02:21:52 -0800

    Change preset date ranges

--- src/stats/stats.js
+++ src/stats/stats.js
@@ -158,12 +158,18 @@ $(document).ready(() => {
 	const momentToday = moment();
 
 	$('input[name="daterange"]').daterangepicker({
+		locale: {
+			format: 'dddd, MMMM Do YYYY'
+		},
+		dateLimit: {
+			months: 1
+		},
 		startDate: momentLastWeek,
 		endDate: momentToday,
 		ranges: {
-			'Today': [moment(), moment()],
-			'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
 			'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+			'This week': [moment().startOf('week'), moment().endOf('week')],
+			'Last week': [moment().subtract(1, 'week').startOf('week'), moment().subtract(1, 'week').endOf('week')],
 			'Last 30 Days': [moment().subtract(29, 'days'), moment()],
 			'This Month': [moment().startOf('month'), moment().endOf('month')],
 			'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
