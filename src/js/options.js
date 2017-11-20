45d28f66aa7c5c43393ce50b9cf189d7a7918f59
Andrei <andrei.dorman@altran.com>
2017-11-20 19:45:24 +0100

    Add settings for the three timers
    Settings page: adds the possibility to set the value of the timers in minutes for the three timers: tomato, short break and long break.

--- src/js/options.js
+++ src/js/options.js
@@ -3,6 +3,9 @@ class Options {
 		this.settings = new Settings();
 
 		this.domNotificationSoundCheckbox = document.getElementById('notification-sound-checkbox');
+		this.domMinutesInTomato = document.getElementById('minutes-in-tomato');
+		this.domMinutesInShortBreak = document.getElementById('minutes-in-short-break');
+		this.domMinutesInLongBreak = document.getElementById('minutes-in-long-break');
 
 		this.setOptionsOnPage();
 		this.setEventListeners();
@@ -10,17 +13,29 @@ class Options {
 
 	setOptionsOnPage() {
 		this.settings.getSettings().then(settings => {
-			const {isNotificationSoundEnabled} = settings;
+			const {isNotificationSoundEnabled, 
+				minutesInTomato,
+				minutesInShortBreak,
+				minutesInLongBreak} = settings;
 
 			this.domNotificationSoundCheckbox.checked = isNotificationSoundEnabled;
+			this.domMinutesInTomato.value = minutesInTomato;
+			this.domMinutesInShortBreak.value = minutesInShortBreak;
+			this.domMinutesInLongBreak.value = minutesInLongBreak;
 		});
 	}
 
 	saveOptions(e) {
 		const isNotificationSoundEnabled = this.domNotificationSoundCheckbox.checked;
+		const minutesInTomato = parseInt(this.domMinutesInTomato.value);
+		const minutesInShortBreak = parseInt(this.domMinutesInShortBreak.value);
+		const minutesInLongBreak = parseInt(this.domMinutesInLongBreak.value);
 
 		this.settings.saveSettings({
-			[SETTINGS_KEY.IS_NOTIFICATION_SOUND_ENABLED]: isNotificationSoundEnabled
+			[SETTINGS_KEY.IS_NOTIFICATION_SOUND_ENABLED]: isNotificationSoundEnabled,
+			[SETTINGS_KEY.MINUTES_IN_TOMATO]: minutesInTomato,
+			[SETTINGS_KEY.MINUTES_IN_SHORT_BREAK]: minutesInShortBreak,
+			[SETTINGS_KEY.MINUTES_IN_LONG_BREAK]: minutesInLongBreak
 		});
 	}
 
