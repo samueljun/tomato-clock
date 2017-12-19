982334288f388422b72fbc3a65e7ca238d4977b3
Samuel Jun <mrsamueljun@gmail.com>
2017-12-19 17:37:30 -0600

    Add try-catch for badge text
    Add try-catch because Firefox Android lacks badge support

--- src/js/badge.js
+++ src/js/badge.js
@@ -8,8 +8,12 @@ class Badge {
 	}
 
 	setBadgeText(text) {
-		browser.browserAction.setBadgeBackgroundColor({color: '#666'});
-		browser.browserAction.setBadgeText({text});
+		// Try-catch because Firefox Android lacks badge support
+		try {
+			browser.browserAction.setBadgeBackgroundColor({color: '#666'});
+			browser.browserAction.setBadgeText({text});
+		} catch (ignoredError) {}
+
 		this.badgeText = text;
 	}
 }
