47fbb428c2204a0f742f31dcf2e6267a7b1b9629
primeapple <toni.mueller@student.uni-halle.de>
2018-01-23 15:45:56 +0100

    made it look a bit more apealing, but I still have no idea how to prevent the notification from closing itself after around 5 seconds

--- src/js/notifications.js
+++ src/js/notifications.js
@@ -4,6 +4,8 @@ class Notifications {
 
 		this.notificationSound = new Audio('/assets/sounds/Portal2_sfx_button_positive.m4a');
 
+		this.notificationClicked = false;
+
 		this.setListeners();
 	}
 
@@ -25,28 +27,33 @@ class Notifications {
 				break;
 		}
 
+		this.lastUsedMessage = message;
+
+		browser.notifications.create(NOTIFICATION_ID, {
+			type: 'basic',
+			iconUrl: '/assets/img/tomato-icon-64.png',
+			title: 'Tomato Clock',
+			//implemented in Chrome but not in Firefox yet
+			//requireInteraction: settings.annoyingMode,
+			message
+		});
+
 		this.settings.getSettings().then(settings => {
-			browser.notifications.create(NOTIFICATION_ID, {
-				type: 'basic',
-				iconUrl: '/assets/img/tomato-icon-64.png',
-				title: 'Tomato Clock',
-				//implemented in Chrome but not in Firefox yet
-				//requireInteraction: settings.annoyingMode,
-				message
-			});
 			if (settings.annoyingMode) {
 				this.notificationSound.loop = true;
 			}
 			if (settings.isNotificationSoundEnabled) {
 				this.notificationSound.play();
 			}
-			
+
 		});
 	}
 
 	setListeners() {
 		browser.notifications.onClicked.addListener(notificationId => {
 			if (notificationId === NOTIFICATION_ID) {
+				console.log("notificationClicked");
+				this.notificationClicked = true;
 				browser.notifications.clear(notificationId);
 				this.notificationSound.pause();
 			}
