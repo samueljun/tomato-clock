ce1c54f0028d919862f5d0c5051c0d0468e79278
Samuel Jun <mrsamueljun@gmail.com>
2019-02-18 23:54:34 -0800

    Add setting to control toolbar badge display
    Closes #50
    Closes #55

--- src/background/badge.js
+++ src/background/badge.js
@@ -1,7 +1,12 @@
 import browser from "webextension-polyfill";
 
+import Settings from "../utils/settings";
+import { SETTINGS_KEY } from "../utils/constants";
+
 export default class Badge {
   constructor() {
+    this.settings = new Settings();
+
     this.badgeText = "";
   }
 
@@ -9,15 +14,25 @@ export default class Badge {
     return this.badgeText;
   }
 
-  setBadgeText(text) {
+  _setBadgeText(text, backgroundColor) {
     // Try-catch because Firefox Android lacks badge support
     try {
-      browser.browserAction.setBadgeBackgroundColor({ color: "#666" });
       browser.browserAction.setBadgeText({ text });
+      browser.browserAction.setBadgeBackgroundColor({ color: backgroundColor });
     } catch (ignoredError) {
       return;
     }
+  }
 
-    this.badgeText = text;
+  setBadgeText(text) {
+    this.settings.getSettings().then(settings => {
+      if (settings[SETTINGS_KEY.IS_TOOLBAR_BADGE_ENABLED]) {
+        this._setBadgeText(text, "#666");
+        this.badgeText = text;
+      } else {
+        this._setBadgeText(null, "#666");
+        this.badgeText = text;
+      }
+    });
   }
 }
--- src/options/options.js
+++ src/options/options.js
@@ -8,15 +8,18 @@ export default class Options {
   constructor() {
     this.settings = new Settings();
 
-    this.domNotificationSoundCheckbox = document.getElementById(
-      "notification-sound-checkbox"
-    );
     this.domMinutesInTomato = document.getElementById("minutes-in-tomato");
     this.domMinutesInShortBreak = document.getElementById(
       "minutes-in-short-break"
     );
     this.domMinutesInLongBreak = document.getElementById(
       "minutes-in-long-break"
+    );
+    this.domNotificationSoundCheckbox = document.getElementById(
+      "notification-sound-checkbox"
+    );
+    this.domToolbarBadgeCheckbox = document.getElementById(
+      "toolbar-badge-checkbox"
     );
 
     this.setOptionsOnPage();
@@ -26,31 +29,35 @@ export default class Options {
   setOptionsOnPage() {
     this.settings.getSettings().then(settings => {
       const {
-        isNotificationSoundEnabled,
         minutesInTomato,
         minutesInShortBreak,
-        minutesInLongBreak
+        minutesInLongBreak,
+        isNotificationSoundEnabled,
+        isToolbarBadgeEnabled
       } = settings;
 
-      this.domNotificationSoundCheckbox.checked = isNotificationSoundEnabled;
       this.domMinutesInTomato.value = minutesInTomato;
       this.domMinutesInShortBreak.value = minutesInShortBreak;
       this.domMinutesInLongBreak.value = minutesInLongBreak;
+      this.domNotificationSoundCheckbox.checked = isNotificationSoundEnabled;
+      this.domToolbarBadgeCheckbox.checked = isToolbarBadgeEnabled;
     });
   }
 
   saveOptions() {
-    const isNotificationSoundEnabled = this.domNotificationSoundCheckbox
-      .checked;
     const minutesInTomato = parseInt(this.domMinutesInTomato.value);
     const minutesInShortBreak = parseInt(this.domMinutesInShortBreak.value);
     const minutesInLongBreak = parseInt(this.domMinutesInLongBreak.value);
+    const isNotificationSoundEnabled = this.domNotificationSoundCheckbox
+      .checked;
+    const isToolbarBadgeEnabled = this.domToolbarBadgeCheckbox.checked;
 
     this.settings.saveSettings({
-      [SETTINGS_KEY.IS_NOTIFICATION_SOUND_ENABLED]: isNotificationSoundEnabled,
       [SETTINGS_KEY.MINUTES_IN_TOMATO]: minutesInTomato,
       [SETTINGS_KEY.MINUTES_IN_SHORT_BREAK]: minutesInShortBreak,
-      [SETTINGS_KEY.MINUTES_IN_LONG_BREAK]: minutesInLongBreak
+      [SETTINGS_KEY.MINUTES_IN_LONG_BREAK]: minutesInLongBreak,
+      [SETTINGS_KEY.IS_NOTIFICATION_SOUND_ENABLED]: isNotificationSoundEnabled,
+      [SETTINGS_KEY.IS_TOOLBAR_BADGE_ENABLED]: isToolbarBadgeEnabled
     });
   }
 
