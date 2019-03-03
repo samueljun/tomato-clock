e9680114700a3639e84732874305e181101c458d
Samuel Jun <mrsamueljun@gmail.com>
2019-03-03 15:34:58 -0800

    Add support for async/await

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,14 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"]
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  useBuiltIns: "usage"
+                }
+              ]
+            ]
           }
         }
       },
