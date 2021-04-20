2fd4ee52fed37db30fdae91c4e700bece0d4d276
gelaber <24670649+LJMP@users.noreply.github.com>
2021-04-20 16:02:22 +0200

    removed erroneous modulo

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -17,7 +17,7 @@ export function getMillisecondsToMinutesAndSeconds(milliseconds) {
 }
 
 export function getMillisecondsToTimeText(milliseconds) {
-  const minutes = parseInt((milliseconds / (1000 * 60)) % 60);
+  const minutes = parseInt(milliseconds / (1000 * 60));
   const seconds = parseInt((milliseconds / 1000) % 60);
   const minutesString = minutes < 10 ? `0${minutes}` : minutes.toString();
   const secondsString = seconds < 10 ? `0${seconds}` : seconds.toString();
