f5244e0c0a68d273d0523b534069bdeeeac9826b
Samuel Jun <mrsamueljun@gmail.com>
2021-04-25 12:12:13 -0400

    WIP

--- src/panel/panel.js
+++ src/panel/panel.js
@@ -58,6 +58,10 @@ export default class Panel {
     document.getElementById("stats-link").addEventListener("click", () => {
       browser.tabs.create({ url: "/stats/stats.html" });
     });
+    document.getElementById("options-link").addEventListener("click", () => {
+      console.log("options-link click");
+      browser.runtime.openOptionsPage();
+    });
   }
 
   resetTimer() {
